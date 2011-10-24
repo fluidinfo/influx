8733e64d05d9fee91b81f32fd6078427630734d2
Jamu Kakar <jkakar@kakar.ca>
2011-10-24 08:38:29 +0300

    - Added logic to pass credentials as command-line options.

--- influx/entrypoint.py
+++ influx/entrypoint.py
@@ -4,7 +4,7 @@ from json import load
 import logging
 from optparse import OptionParser
 
-import fluidinfo as client
+import fluidinfo
 
 from influx.client import FluidinfoImporter
 
@@ -13,18 +13,27 @@ class MissingPathError(Exception):
     """Raised if no file path is provided when the C{influx} tool is run."""
 
 
+class MissingUsernameError(Exception):
+    """Raised if no username is provided when the C{influx} tool is run."""
+
+
+class MissingPasswordError(Exception):
+    """Raised if no password is provided when the C{influx} tool is run."""
+
+
 def main(args):
     """Run the Influx import tool.
 
     @param args: A C{list} of command-line arguments, typically C{sys.argv}.
     """
     options, args = parseOptions(args)
+    client = getFluidinfoClient(options)
     importer = FluidinfoImporter(client, options.batchSize)
     logging.basicConfig(format='%(asctime)s %(levelname)8s  %(message)s',
                         level=logging.INFO)
     for path in args:
-        with open(path, 'r') as dataFile:
-            data = load(dataFile)
+        with open(path, 'r') as file:
+            data = load(file)
             logging.info('Uploading objects from %s' % path)
             importer.upload(data['objects'])
 
@@ -40,7 +49,17 @@ def parseOptions(args):
     options, args = parser.parse_args(args)
     if not args:
         raise MissingPathError('At least one path must be provided.')
+    if not options.username:
+        raise MissingUsernameError('You must provide a username (see -u).')
+    if not options.password:
+        raise MissingPasswordError('You must provide a username (see -u).')
     return options, args
+
+
+def getFluidinfoClient(options):
+    fluidinfo.instance = options.endpoint
+    fluidinfo.login(options.username, options.password)
+    return fluidinfo
 
 
 def getOptionParser():
@@ -51,10 +70,16 @@ def getOptionParser():
     parser = OptionParser()
     parser.usage = 'influx [options] PATH...'
     parser.add_option('-b', dest='batchSize', metavar='BATCH_SIZE', type=int,
-                      help='number of objects to upload per batch '
+                      help='The maximum number of objects to upload per batch '
                            '(default is 100)', default=100)
     parser.add_option('-e', dest='endpoint', metavar='URL',
-                      help='the API endpoint to use (default is '
+                      help='The API endpoint to use (default is '
                            'https://fluiddb.fluidinfo.com)',
                       default='https://fluiddb.fluidinfo.com')
+    parser.add_option('-u', dest='username', metavar='USERNAME',
+                      help='The Fluidinfo username to use when making API '
+                           'calls')
+    parser.add_option('-p', dest='password', metavar='PASSWORD',
+                      help='The Fluidinfo password to use when making API '
+                           'calls')
     return parser
--- influx/tests/test_entrypoint.py
+++ influx/tests/test_entrypoint.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
-from influx.entrypoint import MissingPathError, parseOptions
+from influx.entrypoint import (
+    MissingPathError, MissingUsernameError, MissingPasswordError, parseOptions,
+    getFluidinfoClient)
 
 
 class ParseOptionsTest(TestCase):
@@ -9,17 +11,40 @@ class ParseOptionsTest(TestCase):
         """A L{MissingPathError} is raised if no path is specified."""
         self.assertRaises(MissingPathError, parseOptions, [])
 
+    def testParseOptionsWithoutUsername(self):
+        """A L{MissingUsernameError} is raised if no username is specified."""
+        self.assertRaises(MissingUsernameError, parseOptions,
+                          ['-p', 'secret', 'path'])
+
+    def testParseOptionsWithoutPassword(self):
+        """A L{MissingPasswordError} is raised if no password is specified."""
+        self.assertRaises(MissingPasswordError, parseOptions,
+                          ['-u', 'user', 'path'])
+
     def testParseOptionsUsesSensibleDefaultValues(self):
         """Sensible defaults are provided for all options."""
-        options, args = parseOptions(['path'])
+        options, args = parseOptions(['-u', 'user', '-p', 'secret', 'path'])
         self.assertEqual(100, options.batchSize)
         self.assertEqual('https://fluiddb.fluidinfo.com', options.endpoint)
         self.assertEqual(['path'], args)
 
     def testParseOptions(self):
         """Command-line arguments override default values."""
-        options, args = parseOptions(
-            ['-b', '7', '-e', 'http://localhost:9000', 'path'])
+        options, args = parseOptions(['-u', 'user', '-p', 'secret', '-b', '7',
+                                      '-e', 'http://localhost:9000', 'path'])
+        self.assertEqual('user', options.username)
+        self.assertEqual('secret', options.password)
         self.assertEqual(7, options.batchSize)
         self.assertEqual('http://localhost:9000', options.endpoint)
         self.assertEqual(['path'], args)
+
+
+class GetFluidinfoClientTest(TestCase):
+
+    def testGetFluidinfoClientWithDefaultSettings(self):
+        """The main instance is used by default."""
+        options, args = parseOptions(['-u', 'user', '-p', 'secret', 'path'])
+        client = getFluidinfoClient(options)
+        self.assertEqual('https://fluiddb.fluidinfo.com', client.instance)
+        self.assertEqual('Basic dXNlcjpzZWNyZXQ=',
+                         client.global_headers['Authorization'])
